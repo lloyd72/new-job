5825332fc073dbddea9ffb636cbdd26f492b1009
lloyd72 <122834454+lloyd72@users.noreply.github.com>
2025-01-26 20:29:25 +0000

    Update tracking.js

--- js/tracking.js
+++ js/tracking.js
@@ -1,21 +1,3 @@
-function redirectToTracking() {
-    const trackingNumber = document.getElementById("trackingNumber").value;
-    const trackingPageUrl = 'tracking_' + trackingNumber + '.html';
-
-    // Check if the tracking page exists
-    fetch(trackingPageUrl, { method: 'HEAD' })
-        .then(response => {
-            if (response.ok) {
-                window.location.href = trackingPageUrl;
-            } else {
-                alert('Tracking number not found. Please check and try again.');
-            }
-        })
-        .catch(() => alert('Error occurred while tracking. Please try again.'));
-    
-    return false; // Prevent form submission
-}
-
 window.onload = function() {
     // Get the tracking number from the URL (example: tracking_12345.html)
     const url = window.location.pathname;
